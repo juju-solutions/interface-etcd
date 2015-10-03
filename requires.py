2e25c2e5c297647dfd832982beb0376ecc9180c7
Charles Butler <chuck@dasroot.net>
2015-10-02 21:57:12 -0400

    Renamed provides/requires

---
+++ requires.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from charms.reactive import RelationBase
+from charms.reactive import hook
+from charms.reactive import scopes
+
+
+class EtcdClient(RelationBase):
+    scope = scopes.GLOBAL
+
+    auto_accessors = ['host']
+
+    def port(self):
+        """
+        Get the port.
+
+        If not available, returns the default port of 4001
+        """
+        return self.get_remote('port', 4001)
+
+    @hook('{requires:etcd}-relation-{joined,changed}')
+    def changed(self):
+        self.set_state('{relation_name}.connected')
+        if self.connection_string():
+            self.set_state('{relation_name}.available')
+
+    def connection_string(self):
+        """
+        Get the connection string, if available, or None.
+        """
+        data = {'host': self.host(),
+                'port': self.port()
+                }
+
+        if all(data.values()):
+            return str.format('host={host} port={port}', **data)
+        return None
